c84a62f5756cb81b0847fc32ac0f93609b6f81df
Adam Garberg <adamgarberg@gmail.com>
2022-02-14 22:04:13 -0600

    finished

---
+++ todolist/src/components/Main/Main.jsx
@@ -0,0 +1,33 @@
+function Main (){
+    return(<>   
+
+  <input type="text" placeholder="Input Task Here!"></input>
+
+  <button>Submit</button>
+  <tr>
+        <td>Task</td>
+        <td>Completed?</td>
+        <td></td>
+  </tr>
+    <tr>
+        <td>Take out Dogs </td>
+        <td><button>completed</button></td>
+        <td><button>DELETE</button></td>
+    </tr>
+    <tr>
+    <td>Make Dinner </td>
+    <td><button>completed</button></td>
+    <td><button>DELETE</button></td>
+    </tr>
+    <tr>
+    <td>Go To School </td>
+    <td><button>completed</button></td>
+    <td><button>DELETE</button></td>
+    </tr>
+     </>
+   )
+}
+
+
+
+export default Main;
